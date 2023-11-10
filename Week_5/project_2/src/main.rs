b52442f57a2608a6c9049e1f7012c63c9241dda0
b.dauduCOS101 <basil.daudu@pau.edu.ng>
2023-11-10 14:43:13 +0100

    (Week 5 Commit)

--- Week_5/project_2/src/main.rs
+++ Week_5/project_2/src/main.rs
@@ -1,20 +1,25 @@
 use std::io;
 
 fn main() {
-    let mut input = String::new();
+    let mut input1 = String::new();
+    let mut input2 = String::new();
+
+    println!("Do you have work experience?: ");
+    io::stdin().read_line(&mut input1).expect("Not a valid string");
+    let experience = input1.trim();
 
     println!("\nEnter your age: ");
-    io::stdin().read_line(&mut input).expect("Not a valid string");
-    let age:i32= input.trim().parse().expect("Not a valid number");
+    io::stdin().read_line(&mut input2).expect("Not a valid string");
+    let age:i32= input2.trim().parse().expect("Not a valid number");
 
-    if age >= 40{
-        println!("You're an experienced employee with an incentive of 1,560,000 Naira");
+    if experience == "yes" && age >= 40{
+        println!("You have an incentive of 1,560,000 Naira");
     }
-    else if age >=30 && age <40{
-        println!("You're an experienced employee with an incentive of 1,480,000 Naira");
+    else if experience == "yes" && age >=30 && age <40{
+        println!("You have an incentive of 1,480,000 Naira");
     }
-    else if age < 30{
-        println!("You're an experienced employee with an incentive of 1,300,000 Naira");
+    else if experience == "yes" && age < 30{
+        println!("You have an incentive of 1,300,000 Naira");
     }
     else{
         println!("You're an inexperienced employee");
