be1c2556b84fe3dd4a192fb6a11573e94ec87469
b.dauduCOS101 <basil.daudu@pau.edu.ng>
2023-12-16 19:02:43 +0100

    (Week 10 Commit)

--- Week_10/project-1/src/main.rs
+++ Week_10/project-1/src/main.rs
@@ -1,3 +1,35 @@
+struct Laptop{
+    amount:u32,
+    price:u32
+
+}
+
+impl Laptop{
+    fn total(&self)->f32{
+        (self.amount * self.price) as f32
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let hp = Laptop{
+        amount:3,
+        price:650_000
+    };
+
+    let ibm = Laptop{
+        amount:3,
+        price:755_000
+    };
+
+    let tosh = Laptop{
+        amount:3,
+        price:550_000
+    };
+
+    let dell = Laptop{
+        amount:3,
+        price:850_000
+    };
+
+    println!("Cost for all laptops:\nHP = N{}\nIBM = N{}\nToshiba = N{}\nDell = N{}\nTotal = N{}",hp.total(),ibm.total(),tosh.total(),dell.total(),hp.total()+ibm.total()+tosh.total()+dell.total());
 }
