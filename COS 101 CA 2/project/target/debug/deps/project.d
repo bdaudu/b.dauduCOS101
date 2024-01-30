5ffdf01b18c00b335838248a4eed90148b293c77
bdaudu <147815184+bdaudu@users.noreply.github.com>
2024-01-30 16:56:08 +0100

    Delete COS 101 CA 2/project directory

--- COS 101 CA 2/project/target/debug/deps/project.d
+++
@@ -1,5 +0,0 @@
-C:\Users\EMMA-PC\Documents\b.dauduCOS101\COS 101 CA 2\project\target\debug\deps\project.exe: src\main.rs
-
-C:\Users\EMMA-PC\Documents\b.dauduCOS101\COS 101 CA 2\project\target\debug\deps\project.d: src\main.rs
-
-src\main.rs:
--- COS 101 CA 2/project/target/debug/project.d
+++
@@ -1,1 +0,0 @@
-C:\Users\EMMA-PC\Documents\b.dauduCOS101\COS\ 101\ CA\ 2\project\target\debug\project.exe: C:\Users\EMMA-PC\Documents\b.dauduCOS101\COS\ 101\ CA\ 2\project\src\main.rs
